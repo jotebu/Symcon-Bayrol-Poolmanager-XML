530a9ebf1fa830de41dba542be90b93cc93448b5
jotebu <andy@zwoyczyk.de>
2026-07-04 12:51:00 +0200

    Make XML polling tolerant and add diagnostics

--- BayrolPoolmanagerXML/module.php
+++ BayrolPoolmanagerXML/module.php
@@ -6,22 +6,22 @@ class BayrolPoolmanagerXML extends IPSModule
 
     private const MEASUREMENTS = [
         4001 => ['ident' => 'PH', 'label' => 'pH', 'unit' => 'pH', 'digits' => 2],
-        4008 => ['ident' => 'ChlorineBromine', 'label' => 'Chlor / Brom', 'unit' => 'mg/l', 'digits' => 2],
+        4008 => ['ident' => 'ChlorineBromine', 'label' => 'Chlor / Brom', 'unit' => 'mg_l', 'digits' => 2],
         4022 => ['ident' => 'Redox', 'label' => 'Redox', 'unit' => 'mV', 'digits' => 0],
         4033 => ['ident' => 'Temperature1', 'label' => 'Temperatur T1', 'unit' => 'C', 'digits' => 1],
         4047 => ['ident' => 'Battery', 'label' => 'Batterie', 'unit' => 'V', 'digits' => 2],
         4069 => ['ident' => 'Temperature2', 'label' => 'Temperatur T2', 'unit' => 'C', 'digits' => 1],
         4071 => ['ident' => 'Temperature3', 'label' => 'Temperatur T3', 'unit' => 'C', 'digits' => 1],
-        4077 => ['ident' => 'O2DosedAmount', 'label' => 'O2 dosierte Menge', 'unit' => 'l', 'digits' => 1],
+        4077 => ['ident' => 'O2DosedAmount', 'label' => 'O2 dosierte Menge', 'unit' => 'l', 'digits' => 1]
     ];
 
     private const SETPOINTS = [
         3001 => ['ident' => 'SetpointPH', 'label' => 'Sollwert pH', 'unit' => 'pH', 'digits' => 2],
         3002 => ['ident' => 'LowerAlarmPH', 'label' => 'Untere Alarmgrenze pH', 'unit' => 'pH', 'digits' => 2],
         3003 => ['ident' => 'UpperAlarmPH', 'label' => 'Obere Alarmgrenze pH', 'unit' => 'pH', 'digits' => 2],
-        3017 => ['ident' => 'SetpointChlorineBromine', 'label' => 'Sollwert Chlor / Brom', 'unit' => 'mg/l', 'digits' => 2],
-        3018 => ['ident' => 'LowerAlarmChlorineBromine', 'label' => 'Untere Alarmgrenze Chlor / Brom', 'unit' => 'mg/l', 'digits' => 2],
-        3019 => ['ident' => 'UpperAlarmChlorineBromine', 'label' => 'Obere Alarmgrenze Chlor / Brom', 'unit' => 'mg/l', 'digits' => 2],
+        3017 => ['ident' => 'SetpointChlorineBromine', 'label' => 'Sollwert Chlor / Brom', 'unit' => 'mg_l', 'digits' => 2],
+        3018 => ['ident' => 'LowerAlarmChlorineBromine', 'label' => 'Untere Alarmgrenze Chlor / Brom', 'unit' => 'mg_l', 'digits' => 2],
+        3019 => ['ident' => 'UpperAlarmChlorineBromine', 'label' => 'Obere Alarmgrenze Chlor / Brom', 'unit' => 'mg_l', 'digits' => 2],
         3049 => ['ident' => 'SetpointRedox1', 'label' => 'Sollwert Redox 1', 'unit' => 'mV', 'digits' => 0],
         3050 => ['ident' => 'SetpointRedox2', 'label' => 'Sollwert Redox 2', 'unit' => 'mV', 'digits' => 0],
         3051 => ['ident' => 'LowerAlarmRedox1', 'label' => 'Untere Alarmgrenze Redox 1', 'unit' => 'mV', 'digits' => 0],
@@ -34,40 +34,20 @@ class BayrolPoolmanagerXML extends IPSModule
         3075 => ['ident' => 'UpperAlarmT2', 'label' => 'Obere Alarmgrenze T2', 'unit' => 'C', 'digits' => 1],
         3079 => ['ident' => 'LowerAlarmT3', 'label' => 'Untere Alarmgrenze T3', 'unit' => 'C', 'digits' => 1],
         3080 => ['ident' => 'UpperAlarmT3', 'label' => 'Obere Alarmgrenze T3', 'unit' => 'C', 'digits' => 1],
-        3084 => ['ident' => 'BasicDosingAmountO2', 'label' => 'Grund-Dosiermenge O2', 'unit' => 'l', 'digits' => 1],
+        3084 => ['ident' => 'BasicDosingAmountO2', 'label' => 'Grund-Dosiermenge O2', 'unit' => 'l', 'digits' => 1]
     ];
 
     private const ALARMS = [
-        2001 => 'Sammelalarm',
-        2002 => 'Einschaltverzoegerung',
-        2003 => 'Kein Flow-Signal Eingang Flow',
-        2004 => 'Kein Flow-Signal Eingang IN 1',
-        2005 => 'Oberer Alarm pH',
-        2006 => 'Unterer Alarm pH',
-        2009 => 'Dosier-Alarm pH',
-        2010 => 'Oberer Alarm Chlor / Brom',
-        2011 => 'Unterer Alarm Chlor / Brom',
-        2012 => 'Niveau Alarm Chlor',
-        2013 => 'Niveau-Warnung Chlor',
-        2014 => 'Dosier-Alarm Chlor / Brom',
-        2019 => 'Oberer Alarm Redox',
-        2020 => 'Unterer Alarm Redox',
-        2021 => 'Niveau Alarm Redox',
-        2022 => 'Niveau-Warnung Redox',
-        2023 => 'Dosier-Alarm Redox',
-        2024 => 'Niveau Alarm O2',
-        2025 => 'Niveau-Warnung O2',
-        2028 => 'Oberer Alarm Temperatur T1',
-        2029 => 'Unterer Alarm Temperatur T1',
-        2030 => 'Oberer Alarm Temperatur T2',
-        2031 => 'Unterer Alarm Temperatur T2',
-        2032 => 'Oberer Alarm Temperatur T3',
-        2033 => 'Unterer Alarm Temperatur T3',
-        2034 => 'Batterie-Alarm',
-        2035 => 'Niveau Alarm pH+',
-        2036 => 'Niveau-Warnung pH+',
-        2037 => 'Niveau Alarm pH-',
-        2038 => 'Niveau-Warnung pH-',
+        2001 => 'Sammelalarm', 2002 => 'Einschaltverzoegerung', 2003 => 'Kein Flow-Signal Eingang Flow',
+        2004 => 'Kein Flow-Signal Eingang IN 1', 2005 => 'Oberer Alarm pH', 2006 => 'Unterer Alarm pH',
+        2009 => 'Dosier-Alarm pH', 2010 => 'Oberer Alarm Chlor / Brom', 2011 => 'Unterer Alarm Chlor / Brom',
+        2012 => 'Niveau Alarm Chlor', 2013 => 'Niveau-Warnung Chlor', 2014 => 'Dosier-Alarm Chlor / Brom',
+        2019 => 'Oberer Alarm Redox', 2020 => 'Unterer Alarm Redox', 2021 => 'Niveau Alarm Redox',
+        2022 => 'Niveau-Warnung Redox', 2023 => 'Dosier-Alarm Redox', 2024 => 'Niveau Alarm O2',
+        2025 => 'Niveau-Warnung O2', 2028 => 'Oberer Alarm Temperatur T1', 2029 => 'Unterer Alarm Temperatur T1',
+        2030 => 'Oberer Alarm Temperatur T2', 2031 => 'Unterer Alarm Temperatur T2', 2032 => 'Oberer Alarm Temperatur T3',
+        2033 => 'Unterer Alarm Temperatur T3', 2034 => 'Batterie-Alarm', 2035 => 'Niveau Alarm pH+',
+        2036 => 'Niveau-Warnung pH+', 2037 => 'Niveau Alarm pH-', 2038 => 'Niveau-Warnung pH-',
         2039 => 'Niveau Alarm Flockmatic'
     ];
 
@@ -88,8 +68,7 @@ class BayrolPoolmanagerXML extends IPSModule
         $this->RegisterProfiles();
         $this->RegisterVariables();
 
-        $host = trim($this->ReadPropertyString('Host'));
-        if ($host === '') {
+        if (trim($this->ReadPropertyString('Host')) === '') {
             $this->SetStatus(201);
             $this->SetTimerInterval(self::TIMER_NAME, 0);
             return;
@@ -99,50 +78,87 @@ class BayrolPoolmanagerXML extends IPSModule
         $this->SetStatus(102);
     }
 
-    public function Update(): bool
+    public function Update()
     {
-        $host = trim($this->ReadPropertyString('Host'));
-        if ($host === '') {
+        if (trim($this->ReadPropertyString('Host')) === '') {
             $this->SetStatus(201);
             return false;
         }
 
-        try {
-            foreach (self::MEASUREMENTS as $id => $meta) {
-                $item = $this->ReadItem(34, $id);
-                $this->SetValueSafe($meta['ident'], (float) $item['value']);
-            }
+        $ok = 0;
+        $errors = [];
 
-            if ($this->ReadPropertyBoolean('ReadSetpoints')) {
-                foreach (self::SETPOINTS as $id => $meta) {
-                    $item = $this->ReadItem(34, $id);
-                    $this->SetValueSafe($meta['ident'], (float) $item['value']);
+        foreach (self::MEASUREMENTS as $id => $meta) {
+            if ($this->ReadValueItem(34, $id, $meta['ident'], $errors)) {
+                $ok++;
+            }
+        }
+
+        if ($this->ReadPropertyBoolean('ReadSetpoints')) {
+            foreach (self::SETPOINTS as $id => $meta) {
+                if ($this->ReadValueItem(34, $id, $meta['ident'], $errors)) {
+                    $ok++;
                 }
             }
+        }
 
-            foreach (self::ALARMS as $id => $label) {
-                $item = $this->ReadItem(44, $id);
-                $this->SetValueSafe('AlarmActive' . $id, ((int) $item['active']) === 1);
-                $this->SetValueSafe('AlarmDisplayed' . $id, ((int) $item['displayed']) === 1);
+        foreach (self::ALARMS as $id => $label) {
+            if ($this->ReadAlarmItem($id, $errors)) {
+                $ok++;
             }
+        }
 
-            if ($this->ReadPropertyBoolean('ReadAlarmList')) {
+        if ($this->ReadPropertyBoolean('ReadAlarmList')) {
+            try {
                 $this->SetValueSafe('AlarmList', $this->ReadAlarmListText());
+                $ok++;
+            } catch (Exception $e) {
+                $errors[] = '1.1092: ' . $e->getMessage();
             }
+        }
 
-            $this->SetValueSafe('LastUpdate', time());
-            $this->SetValueSafe('LastError', '');
+        $this->SetValueSafe('LastUpdate', time());
+        $this->SetValueSafe('SuccessfulReads', $ok);
+        $this->SetValueSafe('LastError', implode("\n", array_slice($errors, 0, 25)));
+
+        if ($ok > 0) {
             $this->SetStatus(102);
             return true;
-        } catch (Throwable $e) {
-            $this->SetValueSafe('LastError', $e->getMessage());
-            $this->SetStatus(202);
-            $this->SendDebug('Update failed', $e->getMessage(), 0);
+        }
+
+        $this->SetStatus(202);
+        return false;
+    }
+
+    private function ReadValueItem($type, $id, $ident, &$errors)
+    {
+        try {
+            $item = $this->ReadItem($type, $id);
+            if (!array_key_exists('value', $item)) {
+                throw new Exception('Attribut value fehlt');
+            }
+            $this->SetValueSafe($ident, ((float) $item['value']) / 1);
+            return true;
+        } catch (Exception $e) {
+            $errors[] = $type . '.' . $id . ': ' . $e->getMessage();
             return false;
         }
     }
 
-    private function RegisterVariables(): void
+    private function ReadAlarmItem($id, &$errors)
+    {
+        try {
+            $item = $this->ReadItem(44, $id);
+            $this->SetValueSafe('AlarmActive' . $id, isset($item['active']) && ((int) $item['active']) === 1);
+            $this->SetValueSafe('AlarmDisplayed' . $id, isset($item['displayed']) && ((int) $item['displayed']) === 1);
+            return true;
+        } catch (Exception $e) {
+            $errors[] = '44.' . $id . ': ' . $e->getMessage();
+            return false;
+        }
+    }
+
+    private function RegisterVariables()
     {
         $pos = 10;
         foreach (self::MEASUREMENTS as $meta) {
@@ -158,48 +174,37 @@ class BayrolPoolmanagerXML extends IPSModule
             $this->RegisterVariableBoolean('AlarmDisplayed' . $id, $label . ' angezeigt', '~Alert', $pos++);
         }
         $this->RegisterVariableString('AlarmList', 'Alarmuebersicht', '', 900);
+        $this->RegisterVariableInteger('SuccessfulReads', 'Erfolgreiche XML-Abfragen', '', 997);
         $this->RegisterVariableInteger('LastUpdate', 'Letzte Aktualisierung', '~UnixTimestamp', 998);
-        $this->RegisterVariableString('LastError', 'Letzter Fehler', '', 999);
+        $this->RegisterVariableString('LastError', 'Letzte Fehler / uebersprungene Adressen', '', 999);
     }
 
-    private function RegisterProfiles(): void
+    private function RegisterProfiles()
     {
         $profiles = [
-            ['BPMXML.pH.2', 'pH', 2],
-            ['BPMXML.mg_l.2', 'mg/l', 2],
-            ['BPMXML.mV.0', 'mV', 0],
-            ['BPMXML.C.1', ' C', 1],
-            ['BPMXML.V.2', 'V', 2],
-            ['BPMXML.l.1', 'l', 1],
+            ['BPMXML.pH.2', 'pH', 2], ['BPMXML.mg_l.2', 'mg/l', 2], ['BPMXML.mV.0', 'mV', 0],
+            ['BPMXML.C.1', ' C', 1], ['BPMXML.V.2', 'V', 2], ['BPMXML.l.1', 'l', 1]
         ];
         foreach ($profiles as $profile) {
-            [$name, $suffix, $digits] = $profile;
-            if (!IPS_VariableProfileExists($name)) {
-                IPS_CreateVariableProfile($name, 2);
+            if (!IPS_VariableProfileExists($profile[0])) {
+                IPS_CreateVariableProfile($profile[0], 2);
             }
-            IPS_SetVariableProfileDigits($name, $digits);
-            IPS_SetVariableProfileText($name, '', ' ' . $suffix);
+            IPS_SetVariableProfileDigits($profile[0], $profile[2]);
+            IPS_SetVariableProfileText($profile[0], '', ' ' . $profile[1]);
         }
     }
 
-    private function ProfileName(string $unit): string
+    private function ProfileName($unit)
     {
-        return match ($unit) {
-            'pH' => 'BPMXML.pH.2',
-            'mg/l' => 'BPMXML.mg_l.2',
-            'mV' => 'BPMXML.mV.0',
-            'C' => 'BPMXML.C.1',
-            'V' => 'BPMXML.V.2',
-            'l' => 'BPMXML.l.1',
-            default => ''
-        };
+        $map = ['pH' => 'BPMXML.pH.2', 'mg_l' => 'BPMXML.mg_l.2', 'mV' => 'BPMXML.mV.0', 'C' => 'BPMXML.C.1', 'V' => 'BPMXML.V.2', 'l' => 'BPMXML.l.1'];
+        return isset($map[$unit]) ? $map[$unit] : '';
     }
 
-    private function ReadItem(int $type, int $id): array
+    private function ReadItem($type, $id)
     {
         $xml = $this->FetchXml($type, $id);
         if (!isset($xml->item)) {
-            throw new Exception('XML enthaelt kein item fuer ' . $type . '.' . $id);
+            throw new Exception('XML enthaelt kein item');
         }
         $attributes = $xml->item->attributes();
         $result = [];
@@ -209,7 +214,7 @@ class BayrolPoolmanagerXML extends IPSModule
         return $result;
     }
 
-    private function ReadAlarmListText(): string
+    private function ReadAlarmListText()
     {
         $xml = $this->FetchXml(1, 1092);
         $lines = [];
@@ -222,7 +227,7 @@ class BayrolPoolmanagerXML extends IPSModule
         return implode("\n", $lines);
     }
 
-    private function FetchXml(int $type, int $id): SimpleXMLElement
+    private function FetchXml($type, $id)
     {
         $host = trim($this->ReadPropertyString('Host'));
         $host = preg_replace('#^https?://#', '', $host);
@@ -237,16 +242,24 @@ class BayrolPoolmanagerXML extends IPSModule
         ]);
         $raw = @file_get_contents($url, false, $context);
         if ($raw === false || trim($raw) === '') {
-            throw new Exception('Keine Antwort von ' . $url);
+            throw new Exception('keine HTTP-Antwort');
+        }
+        $raw = trim($raw);
+        $xmlStart = strpos($raw, '<');
+        if ($xmlStart === false) {
+            throw new Exception('Antwort ist kein XML: ' . substr($raw, 0, 100));
+        }
+        if ($xmlStart > 0) {
+            $raw = substr($raw, $xmlStart);
         }
         $xml = @simplexml_load_string($raw);
         if (!$xml instanceof SimpleXMLElement) {
-            throw new Exception('Ungueltige XML-Antwort von ' . $url . ': ' . substr($raw, 0, 120));
+            throw new Exception('ungueltiges XML: ' . substr($raw, 0, 120));
         }
         return $xml;
     }
 
-    private function SetValueSafe(string $ident, mixed $value): void
+    private function SetValueSafe($ident, $value)
     {
         $id = @$this->GetIDForIdent($ident);
         if ($id !== false) {
